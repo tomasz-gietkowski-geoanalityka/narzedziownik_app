7e4aa9e0998845d4eae45bf1e5d70620b43da6e5
Tomasz Giętkowski - Geoanalityka <tomasz.gietkowski@geoanalityka.pl>
2025-11-02 21:56:46 +0100

    folder update

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .narzedziownik_app import NarzedziownikAPP
-
-def classFactory(iface):
-    return NarzedziownikAPP(iface)
--- features/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# Pozwala QGIS importować moduły z katalogu features.
